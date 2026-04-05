3cc9a81cd0dc9c6b0d2dbb06387b78405f3469e3
Hilanmoke <hilanmoke@Mac.attlocal.net>
2026-04-05 00:34:52 -0700

    update README.md

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import './App.css'
 function App() {
-  const todlist =[
+  const todoList =[
     {id: 1, title: 'review resources'},
     {id: 2, title: 'take notes'},
     {id: 3, title:'code out app'},
   ]
-
 return (
 <div>
   <h1>My Todos</h1>
-  <ul> {todlist.map((todo) => ( <li key={todo.id}>{todo.title}</li> ))} </ul> 
-
+  <ul> 
+  {todoList.map((todo) => ( <li key={todo.id}>{todo.title}</li> 
+))}
+ </ul> 
 </div>
 )
 }
