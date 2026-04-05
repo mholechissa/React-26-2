b685f7dc2b5d0b25f04baaf64627733bc1c3fe65
Hilanmoke <hilanmoke@Mac.attlocal.net>
2026-04-04 21:44:21 -0700

    Initial commit - React todo map

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+function App() {
+  const todlist =[
+    {id: 1, title: 'review resources'},
+    {id: 2, title: 'take notes'},
+    {id: 3, title:'code out app'},
+  ]
+
+return (
+<div>
+  <h1>My Todos</h1>
+  <ul> {todlist.map((todo) => ( <li key={todo.id}>{todo.title}</li> ))} </ul> 
+
+</div>
+)
+}
+export default App
