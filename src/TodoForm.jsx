043aa691452d02c783032f5e373a307af816daf4
Mekonnen Holechissa <mekonnenholechissa@gmail.com>
2026-04-26 23:40:31 -0700

    fix component imports and finalize App.jsx todo logic

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,10 +1,34 @@
-function TodoForm() {
-    return(
-        <form>
-            <label htmlFor="todoTitle">Add Todo</label>
-            <input id="todoTitle" type="text" />
-            <button type="submit"disabled>Add</button>
-        </form>
-    );
+import { useRef } from "react";
+
+function TodoForm({ onAddTodo }) {
+  const inputRef = useRef();
+
+  const handleTodo = (event) => {
+    event.preventDefault();
+    const todoTitle = event.target.todoTitle.value.trim();
+
+    if (todoTitle) {
+      onAddTodo(todoTitle);
+      event.target.reset();
+      inputRef.current.focus();
+    }
+  };
+
+  return (
+    <form onSubmit={handleTodo}>
+      <label htmlFor="todoTitle">Add Todo:</label>
+
+      <input
+        ref={inputRef}
+        type="text"
+        id="todoTitle"
+        placeholder="Todo text"
+        required
+      />
+
+      <button type="submit">Add New Todo</button>
+    </form>
+  );
 }
+
 export default TodoForm;
