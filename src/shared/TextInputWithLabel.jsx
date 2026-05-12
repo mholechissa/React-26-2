f79ec1a0179f4bcf1518c4b2325f17ce19e43e2c
Mekonnen Holechissa <mekonnenholechissa@gmail.com>
2026-05-11 23:32:47 -0700

    Create reusable TextInputWithLabel component

---
+++ src/shared/TextInputWithLabel.jsx
@@ -0,0 +1,23 @@
+function TextInputWithLabel({
+  elementId,
+  labelText,
+  onChange,
+  ref,
+  value,
+}) {
+  return (
+    <>
+      <label htmlFor={elementId}>{labelText}</label>
+
+      <input
+        type="text"
+        id={elementId}
+        ref={ref}
+        value={value}
+        onChange={onChange}
+      />
+    </>
+  );
+}
+
+export default TextInputWithLabel;
